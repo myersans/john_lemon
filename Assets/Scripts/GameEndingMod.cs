ff356794bd18a4213842ea1dee4c273e5eff9997
andy myers <myersans@iu.edu>
2022-03-22 14:17:05 -0400

    worked on a new game ending script, there are a few issues thus far to resolve
    1. Now my images wont display

--- Assets/Scripts/GameEndingMod.cs
+++ Assets/Scripts/GameEndingMod.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameEnding : MonoBehaviour
+public class GameEndingMod : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
@@ -13,18 +13,9 @@ public class GameEnding : MonoBehaviour
     public CanvasGroup caughtBackgroundImageCanvasGroup;
     public AudioSource caughtAudio;
 
-    bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
     bool m_HasAudioPlayed;
-    
-    void OnTriggerEnter (Collider other)
-    {
-        if (other.gameObject == player)
-        {
-            m_IsPlayerAtExit = true;
-        }
-    }
 
     public void CaughtPlayer ()
     {
@@ -33,11 +24,7 @@ public class GameEnding : MonoBehaviour
 
     void Update ()
     {
-        if (m_IsPlayerAtExit)
-        {
-            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
-        }
-        else if (m_IsPlayerCaught)
+        if (m_IsPlayerCaught)
         {
             EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
